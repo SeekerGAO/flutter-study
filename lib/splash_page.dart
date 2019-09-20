68f8e99cb21efefc71418a6368e4dff6926c20fa
SeekerGAO <1532584997@qq.com>
2019-09-21 01:01:53 +0800

    新增启动页面

---
+++ lib/splash_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+/**
+ * 启动页面
+ */
+class SplashPage extends StatefulWidget{
+  SplashPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SplashPageState();
+  }
+}
+
+class _SplashPageState extends State<SplashPage>{
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Builder(builder: (context){
+      return Container(
+        child: Image(image: AssetImage("assets/images/ic_launcher.png"), fit: BoxFit.fill,),
+      );
+    });
+  }
+}
