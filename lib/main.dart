eea3e5a9fd3d8a286a422348cb596b8ac2478154
SeekerGAO <1532584997@qq.com>
2019-09-23 22:25:28 +0800

    基础组件-按钮

--- lib/main.dart
+++ lib/main.dart
@@ -412,6 +412,25 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
 
             TextFonts(),
+
+            FlatButton(
+              child: Text("Submit"),
+              color: Colors.blue,
+              highlightColor: Colors.blue[700],
+              colorBrightness: Brightness.dark,
+              splashColor: Colors.grey,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+              onPressed: (){},
+            ),
+            RaisedButton(
+              child: Text("Submit"),
+              color: Colors.blue,
+              highlightColor: Colors.blue[700],
+              colorBrightness: Brightness.dark,
+              splashColor: Colors.grey,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+              onPressed: (){},
+            )
           ],
         ),
       ),
