0dea70b7a0157166dc75b5c93c46fd8b9e8f628b
SeekerGAO <1532584997@qq.com>
2019-09-26 22:39:16 +0800

    容器类组件-Container容器

--- lib/main.dart
+++ lib/main.dart
@@ -667,6 +667,33 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
               ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 100.0),
+                child: Container(
+                  margin: EdgeInsets.only(top: 50.0, left: 120.0), //容器外填充
+                  constraints: BoxConstraints.tightFor(width: 200.0, height: 150.0), //卡片大小
+                  decoration: BoxDecoration(//背景装饰
+                      gradient: RadialGradient( //背景径向渐变
+                          colors: [Colors.red, Colors.orange],
+                          center: Alignment.topLeft,
+                          radius: .98
+                      ),
+                      boxShadow: [ //卡片阴影
+                        BoxShadow(
+                            color: Colors.black54,
+                            offset: Offset(2.0, 2.0),
+                            blurRadius: 4.0
+                        )
+                      ]
+                  ),
+                  transform: Matrix4.rotationZ(.2), //卡片倾斜变换
+                  alignment: Alignment.center, //卡片内文字居中
+                  child: Text( //卡片文字
+                    "卡片", style: TextStyle(color: Colors.white, fontSize: 40.0),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
