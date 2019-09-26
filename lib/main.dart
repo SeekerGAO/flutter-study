bdc45652201c8152a643f703a7b5979d5b8c71f5
SeekerGAO <1532584997@qq.com>
2019-09-26 22:32:45 +0800

    容器类组件-变换（Transform）

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'dart:math' as math;
 
 import 'counter_page.dart';
 
@@ -580,7 +581,92 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: Text("Login", style: TextStyle(color: Colors.white),),
                     )
                 ),
-              )
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50.0),
+                child: Container(
+                  color: Colors.black,
+                  child: new Transform(
+                    alignment: Alignment.topRight, //相对于坐标系原点的对齐方式
+                    transform: new Matrix4.skewY(0.4), //沿Y轴倾斜0.3弧度
+                    child: new Container(
+                      padding: const EdgeInsets.all(8.0),
+                      color: Colors.deepOrange,
+                      child: const Text('变换（Transform）!'),
+                    ),
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50.0),
+                child: DecoratedBox(
+                  decoration:BoxDecoration(color: Colors.red),
+                  //默认原点为左上角，左移20像素，向上平移5像素
+                  child: Transform.translate(
+                    offset: Offset(-20.0, -5.0),
+                    child: Text("平移"),
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50.0),
+                child: DecoratedBox(
+                  decoration:BoxDecoration(color: Colors.red),
+                  child: Transform.rotate(
+                    //旋转90度
+                    angle:math.pi/2 ,
+                    child: Text("旋转"),
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50.0),
+                child: DecoratedBox(
+                    decoration:BoxDecoration(color: Colors.red),
+                    child: Transform.scale(
+                        scale: 1.5, //放大到1.5倍
+                        child: Text("缩放")
+                    )
+                ),
+                ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    DecoratedBox(
+                        decoration:BoxDecoration(color: Colors.red),
+                        child: Transform.scale(scale: 1.5,
+                            child: Text("Transform的变换是应用在绘制阶段")
+                        )
+                    ),
+                    Text("你好", style: TextStyle(color: Colors.green, fontSize: 18.0),)
+                  ],
+                )
+              ),
+
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      DecoratedBox(
+                        decoration: BoxDecoration(color: Colors.red),
+                        //将Transform.rotate换成RotatedBox
+                        child: RotatedBox(
+                          quarterTurns: 1, //旋转90度(1/4圈)
+                          child: Text("RotatedBox是作用于layout阶段，所以子组件会旋转90度（而不只是绘制的内容"),
+                        ),
+                      ),
+                      Text("你好", style: TextStyle(color: Colors.green, fontSize: 18.0),)
+                    ],
+                  ),
+              ),
             ],
           ),
         ),
