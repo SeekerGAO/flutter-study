fbda88136b6a8b52cc45cd08a493448b349fca82
SeekerGAO <1532584997@qq.com>
2019-09-26 22:13:26 +0800

    容器类组件-装饰容器（DecoratedBox）

--- lib/main.dart
+++ lib/main.dart
@@ -561,6 +561,26 @@ class _MyHomePageState extends State<MyHomePage> {
                   Navigator.pushNamed(context, "constrainedbox_page");
                 },
               ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors:[Colors.red,Colors.orange[700]]), //背景渐变
+                        borderRadius: BorderRadius.circular(3.0), //3像素圆角
+                        boxShadow: [ //阴影
+                          BoxShadow(
+                              color:Colors.black54,
+                              offset: Offset(2.0,2.0),
+                              blurRadius: 4.0
+                          )
+                        ]
+                    ),
+                    child: Padding(padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 18.0),
+                      child: Text("Login", style: TextStyle(color: Colors.white),),
+                    )
+                ),
+              )
             ],
           ),
         ),
